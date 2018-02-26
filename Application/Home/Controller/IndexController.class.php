8e024ad69141e2451a4b6adbf2ea384cdac6e165
Your Name <you@example.com>
2018-02-26 15:55:30 +0800

    修改

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -8,16 +8,13 @@ use Think\Model;
 
 class IndexController extends Controller
 {
-
     public function index()
     {
-
-        echo 111;exit;
-
         $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
         $uachar = "/(nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|mobile)/i";
         $model = M('ShareList as sl');
-        $data['data'] = $model->field('sl.*,u.*')->join('left join bdp_user as u on u.id=sl.user_id')->select();
+        $filed = "sl.id as sl_id,sl.face,sl.face,sl.name,sl.number,sl.create_time,sl.link,u.*";
+        $data['data'] = $model->field($filed)->join('left join bdp_user as u on u.id=sl.user_id')->select();
         $data['count'] = $model->join('left join bdp_user as u on u.id=sl.user_id')->count();
         $data['info'] = $model->find();
         foreach ($data['data'] as $key => $value) {
@@ -35,8 +32,6 @@ class IndexController extends Controller
                 'count' => $data['count'],
                 'info' => $data['info']
             ))->display();
-
         }
     }
-
 }
--- Application/Home/Controller/ShareController.class.php
+++ Application/Home/Controller/ShareController.class.php
@@ -10,68 +10,63 @@ class ShareController extends Controller
 {
     public function add()
     {
-        if (!$data = S('share_add')) {
-            ini_set('user_agent', 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19');
-            if (!session('home_login_id')) $this->ajaxReturn(array('status' => 0, 'msg' => '请登录后再分享!'));
-            $links = isset($_POST['short']) && $_POST['short'] != '' ? explode(",", trim($_POST['short'])) : "";
-            foreach ($links as $value) {
-                if (strpos($value, 'http') > 0) {
-                    $value = explode('http', $value);
-                    if ($value[1]) {
-                        $value = 'http' . $value[1];
-                    }
+        ini_set('user_agent', 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19');
+        if (!session('home_login_id')) $this->ajaxReturn(array('status' => 0, 'msg' => '请登录后再分享!'));
+        $links = isset($_POST['short']) && $_POST['short'] != '' ? explode(",", trim($_POST['short'])) : "";
+        foreach ($links as $value) {
+            if (strpos($value, 'http') > 0) {
+                $value = explode('http', $value);
+                if ($value[1]) {
+                    $value = 'http' . $value[1];
                 }
             }
-            $model = M('ShareList');
-            $data['count_link'] = $model->where(array('link' => $value))->find();
-            if ($data['count_link'] > 0) $this->ajaxReturn(array('status' => 0, 'msg' => '该链接已经存在!'));
-            $data['content'] = file_get_contents($value);
-            if ($data['content']) {
-                $data['content'] = str_replace("'", "", $this->autoiconv($data['content']));
-                $rule_group_member = 'FileUtils.g_name="@@@";FileUtils.u_avator_list';
-                $rule_group_member_number = '<span id="group-user-num">@@@</span>';
-                $rule_group_member_avatar = 'FileUtils.u_avator_list=JSON.parse(@@@);FileUtils.mobileModel="Android";';
-                preg_match("/" . $this->restr($rule_group_member) . "/is", $data['content'], $regm);
-                $group_member = $this->fstr(strip_tags($regm[1]), $this->restr($rule_group_member, 'f'));
-                if (strpos($group_member, '*')) {
-                    $group_member = str_replace("*", "-", $group_member);
-                } else {
-                    $len = strlen($group_member);
-                    $group_member = substr($group_member, 0, ($len > 15 ? 6 : 3)) . "----" . substr($group_member, $len - ($len > 15 ? 6 : 3), $len);
-                }
-                preg_match("/" . $this->restr($rule_group_member_number) . "/is", $data['content'], $regm);
-                $group_number = $this->fstr(strip_tags($regm[1]), $this->restr($rule_group_member_number, 'f'));
-                preg_match("/" . $this->restr($rule_group_member_avatar) . "/is", $data['content'], $regm);
-                $group_avatar = $this->fstr(strip_tags($regm[1]), $this->restr($rule_group_member_avatar, 'f'));
-                $group_avatar = str_replace(array("\\", '("', '")'), array("", "", ""), $group_avatar);
-                $rowjson = json_decode($group_avatar);
-                $group_avatars = array();
-                foreach ($rowjson as $value1) {
-                    $group_avatars[] = $value1->photo;
-                }
-                $group_avatar_str = implode(",", $group_avatars);
-                if ($group_member && $group_number > 0) {
-                    $insert_arr = array(
-                        'face' => $group_avatar_str,
-                        'name' => $group_member,
-                        'number' => $group_number,
-                        'link' => $value,
-                        'create_time' => time(),
-                        'user_id' => session('home_login_id')
-                    );
-                    $data = $model->add($insert_arr);
-                    if ($data) {
-                        $this->ajaxReturn(array('status' => 200, 'msg' => '提交成功'));
-                    }
-                } else {
-                    $this->ajaxReturn(array('status' => 0, 'msg' => '分享失败！请检查链接是否正确或重复'));
+        }
+        $model = M('ShareList');
+        $data['count_link'] = $model->where(array('link' => $value))->find();
+        if ($data['count_link'] > 0) $this->ajaxReturn(array('status' => 0, 'msg' => '该链接已经存在!'));
+        $data['content'] = file_get_contents($value);
+        if ($data['content']) {
+            $data['content'] = str_replace("'", "", $this->autoiconv($data['content']));
+            $rule_group_member = 'FileUtils.g_name="@@@";FileUtils.u_avator_list';
+            $rule_group_member_number = '<span id="group-user-num">@@@</span>';
+            $rule_group_member_avatar = 'FileUtils.u_avator_list=JSON.parse(@@@);FileUtils.mobileModel="Android";';
+            preg_match("/" . $this->restr($rule_group_member) . "/is", $data['content'], $regm);
+            $group_member = $this->fstr(strip_tags($regm[1]), $this->restr($rule_group_member, 'f'));
+            if (strpos($group_member, '*')) {
+                $group_member = str_replace("*", "-", $group_member);
+            } else {
+                $len = strlen($group_member);
+                $group_member = substr($group_member, 0, ($len > 15 ? 6 : 3)) . "----" . substr($group_member, $len - ($len > 15 ? 6 : 3), $len);
+            }
+            preg_match("/" . $this->restr($rule_group_member_number) . "/is", $data['content'], $regm);
+            $group_number = $this->fstr(strip_tags($regm[1]), $this->restr($rule_group_member_number, 'f'));
+            preg_match("/" . $this->restr($rule_group_member_avatar) . "/is", $data['content'], $regm);
+            $group_avatar = $this->fstr(strip_tags($regm[1]), $this->restr($rule_group_member_avatar, 'f'));
+            $group_avatar = str_replace(array("\\", '("', '")'), array("", "", ""), $group_avatar);
+            $rowjson = json_decode($group_avatar);
+            $group_avatars = array();
+            foreach ($rowjson as $value1) {
+                $group_avatars[] = $value1->photo;
+            }
+            $group_avatar_str = implode(",", $group_avatars);
+            if ($group_member && $group_number > 0) {
+                $insert_arr = array(
+                    'face' => $group_avatar_str,
+                    'name' => $group_member,
+                    'number' => $group_number,
+                    'link' => $value,
+                    'create_time' => time(),
+                    'user_id' => session('home_login_id')
+                );
+                $data = $model->add($insert_arr);
+                if ($data) {
+                    $this->ajaxReturn(array('status' => 200, 'msg' => '提交成功'));
                 }
             } else {
                 $this->ajaxReturn(array('status' => 0, 'msg' => '分享失败！请检查链接是否正确或重复'));
             }
-
-            S('share_add', $data, array('type' => 'file', 'expire' => 3600));
-
+        } else {
+            $this->ajaxReturn(array('status' => 0, 'msg' => '分享失败！请检查链接是否正确或重复'));
         }
     }
 
@@ -99,8 +94,12 @@ class ShareController extends Controller
             $sql = "update bdp_user set jump_count = '" . $today . ":1' where id =" . $user_id;
             $model->execute($sql);
         }
-        $this->assign('url', $info['link']);
-        $this->display('index/walt');
+        $this->walt($info);
+    }
+
+    public function walt($info)
+    {
+        $this->assign('url', $info['link'])->display('Index/walt');
     }
 
     public function autoiconv($str, $type = "gb2312//IGNORE")
